353652f2eb52c658ec0ab8b809e4c93029cda43d
zaha <zahadhamov@gmail.com>
2020-01-13 19:04:32 +0300

    post class + ancestors

---
+++ memo.rb
@@ -0,0 +1,10 @@
+class Memo < Post
+
+	def read_from_console
+		
+	end
+
+	def to_strings
+		
+	end
+end
---
+++ post.rb
@@ -0,0 +1,33 @@
+class Post
+
+	def initialize
+	@craeted_at = Time.now
+	@text = nil
+	end
+
+	def read_from_console
+		
+	end
+
+	def to_strings
+
+		
+	end
+
+	def save
+		file = File.new(file_path + "w:UTF-8")
+
+		for item in to_strings do 
+			file.puts(item)
+		end
+		file.close
+	end
+
+	def file_path
+		current_path = File.dirname(__FILE__)
+
+		file_name = @craeted_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+		return current_path + "/" + file_name
+		
+	end
+end
